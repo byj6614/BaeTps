abe2747525d560abd1ef9b57d398ec7ff84388e9
영준 배 <byj6614@naver.com>
2023-06-03 23:20:37 +0900

    feat : 0602과제

--- Assets/0602Home/HomePoolManager.cs
+++ Assets/0602Home/HomePoolManager.cs
@@ -1,18 +1,130 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class HomePoolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<string, ObjectPool<GameObject>> poolDic; //pool의 오브젝트를 담는 Dictionary 생성
+    Dictionary<string, Transform> poolContainer;        //pool의 폴더
+    Transform poolRoot;                                 //pool위 위치
+
+    private void Awake()//초기화
     {
-        
+        poolDic=new Dictionary<string, ObjectPool<GameObject>>();
+        poolContainer=new Dictionary<string, Transform>();
+        poolRoot = new GameObject("PoolRoot").transform;
     }
 
-    // Update is called once per frame
-    void Update()
+    public T Get<T>(T original, Vector3 position, Quaternion rotation, Transform parent) where T : Object
+        //T형,포지션,회전,부모 를 받는다.
+    {//T형으로 GameObject와 Component를 받는 Get생성 wherer로 Object형만 받도로 제한
+        if (original is GameObject)
+        {//original이 GameeObject일 때
+            GameObject prefab = original as GameObject;//original을 T형에서 GameObject로 바꿔준다.
+            string key = prefab.name;//현재 오브젝트에 이름을 key에 담아준다.
+
+            if (!poolDic.ContainsKey(key))//poolDic에 key가 없다면 새로 생성해준다.
+                CreatePool(key, prefab);
+            GameObject obj = poolDic[key].Get();//poolDic에서 지정된 obj를 꺼내온다.
+            obj.transform.parent = parent;//부모를 지정해주고
+            obj.transform.position = position;//위치를 지정하고
+            obj.transform.rotation = rotation;//회전을 지정해준다.
+            return obj as T;//마무리로 obj를 T형으로 전환하고 내보내준다.
+        }
+        else if (original is Component)//original이 Component일 경우 
+        {
+            Component comp = original as Component;//original을 Component로 바꿔준다.
+            string key = comp.gameObject.name;//받아온 Component의 이름을 key에 저장
+
+            if (!poolDic.ContainsKey(key))//현재 poolDic에 key가 없을 시 새로 생성해준다.
+                CreatePool(key, comp.gameObject);//현재 컴포넌트의 오브젝트를 넣어준다는것이 위와 다른점
+
+            GameObject obj = poolDic[key].Get();//poolDic에서 지정된 obj를 꺼내온다
+            obj.transform.parent = parent;//부모를 지정해주고
+            obj.transform.position = position;//위치를 지정하고
+            obj.transform.rotation = rotation;//회전을 지정해준다.
+            return obj as T;//마무리로 obj를 T형으로 전환하고 내보내준다.
+        }
+        else
+        {
+            return null;//혹여나 Object에서 넣어야할게 아닌 다른것들이 들어갔을경우 null을 반환해준다.
+        }
+    }
+    public T Get<T>(T original, Vector3 position, Quaternion rotation) where T : Object//부모를 지정받지 않고 함수를 실행할때
     {
-        
+        return Get<T>(original, position, rotation, null);
+    }
+
+    public T Get<T>(T original, Transform parent) where T : Object//위치와 회전을 받지않고 함수를 실행할때
+    {
+        return Get<T>(original, Vector3.zero, Quaternion.identity, parent);
+    }
+
+    public T Get<T>(T original) where T : Object//T형(GameObject와 Component)만 받았을 때
+    {
+        return Get<T>(original, Vector3.zero, Quaternion.identity, null);
+    }
+
+    public bool Release<T>(T instance) where T : Object     //이미 만들어져잇는 T형을 없애는 함수
+    {
+        if(instance is GameObject)//GameObject일경우
+        {
+            GameObject go = instance as GameObject;//instance를 GameObject로 바꿔준다.
+            string key = go.name;  //key에 instance의 이름을 담아준다.
+            
+            if(!poolDic.ContainsKey(key))//poolDic안에 key를 담당하는 게 없을경우 flase를 내보낸다.
+                return false;
+            poolDic[key].Release(go);//poolDic안에 있을경우 Release로 지워준다.
+            return true;//지우는걸 성공했으니 true를 반환한다.
+        }
+        else if(instance is Component)//instance를 Component로 바꿔준다.
+        {
+            Component component = instance as Component;//instance를 Component로 바꿔준다.
+            string key = component.gameObject.name;//key에 Component를 가지고 있는 object의 이름을 담아준다.
+
+            if (!poolDic.ContainsKey(key))//poolDic안에 key를 담당하는 게 없을경우 flase를 내보낸다.
+                return false;
+
+            poolDic[key].Release(component.gameObject);//poolDic안에 있을경우 Release로 지워준다.
+            return true;//지우는걸 성공했으니 true를 반환한다.
+        }
+        else
+        {
+            return false;//GameObject와 Componet말고 다른 형태의 데이터를 받을경우 false를 반환
+        }
+    }
+
+    
+    private void CreatePool(string key, GameObject prefab)//새로운 pool을 만들어주는 함수
+    {
+        GameObject root = new GameObject(); //root를 초기화해준다.
+        root.gameObject.name = $"{key}Container";//새로 만들어지는 pool의 이름
+        root.transform.parent = poolRoot;//새로만들어지는 pool의 부모
+        poolContainer.Add(key, root.transform);//poolContainer라는 폴더안에 생성
+
+        ObjectPool<GameObject> pool = new ObjectPool<GameObject>(//pool을 만들지 삭제할지 활성화할지 비활성화 하는 함수를 람다식으로 지정해준다.
+            createFunc: ()=>//새로운 pool 생성
+            {
+                GameObject obj=Instantiate(prefab);//prefab을 새로이 생성
+                obj.gameObject.name=key;//생성된 prefab이 obj에 들어감과 이름을 새로이 지정
+                return obj;//생성완료
+            },
+            actionOnGet: (GameObject obj)=>//pool을 활성화
+            {
+                obj.gameObject.SetActive(true);//비활성화 된 상태인 obj를 활성화 해준다.
+                obj.transform.parent = null;//부모에서 빠져나온다.
+            },
+            actionOnRelease: (GameObject obj) =>
+            {
+                obj.gameObject.SetActive(false);//활성화 된 상태인 obj를 다시 비활성화 해준다.
+                obj.transform.parent = poolContainer[key];//부모에서 빠져나온 obj를 다시 부모로 넣어준다.
+            },
+            actionOnDestroy: (GameObject obj)=>
+            {
+                Destroy(obj);//오브젝트를 삭제시킨다.
+            }
+            );
+        poolDic.Add(key, pool);//생성된 pool를 넣어준다.
     }
 }
--- Assets/0602Home/HomeResourceManager.cs
+++ Assets/0602Home/HomeResourceManager.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class HomeResourceManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Dictionary<string, Object> resources = new Dictionary<string, Object>();//resources를 담는 Dictionary를 생성
+
+    public T Load<T>(string path) where T : Object//T형을 불러오는 함수.
     {
-        
+        string key = $"{typeof(T)}.{path}";//key의 이름을 넣어준다. 
+
+        if (resources.ContainsKey(key))//Dictionary에 key가 담당하는 Object가 없을경우
+            return resources[key] as T;//새로이 만들어준다.
+
+        T resource = Resources.Load<T>(path);//리소스를 불러와서 resource에 담아준다.
+        resources.Add(key, resource);//resources에 새로이 불러운 resource를 담아준다.
+        return resource;//resource를 내보낸다.
+    }
+    public T Instantiate<T>(T original, Vector3 position, Quaternion rotation, Transform parent, bool pooling = false) where T : Object
+    {//새로이 추가 하는 함수 (T형 origina, 위치, 회전 , 부모 ,pool=false(비활성화를 미리 선언)) Object로 제한해주며
+        if (pooling)//pooling이 true일 경우 
+            return GameManager.Pool.Get(original, position, rotation, parent);//게임 매니저에 있는 Pool을 가져온다.
+        else//아닌경우
+            return Object.Instantiate(original, position, rotation, parent);//새로이 생성해준다.
+    }
+    //부모를 지정안하는 경우
+    public T Instantiate<T>(T original, Vector3 position, Quaternion rotation, bool pooling = false) where T : Object
+    {
+        return Instantiate<T>(original, position, rotation, null, pooling);
+    }
+    //회전과 위치를 지정 안하는 경우
+    public new T Instantiate<T>(T original, Transform parent, bool pooling = false) where T : Object
+    {
+        return Instantiate<T>(original, Vector3.zero, Quaternion.identity, parent, pooling);
+    }
+    //T형만 지정하는 경우
+    public T Instantiate<T>(T original, bool pooling = false) where T : Object
+    {
+        return Instantiate<T>(original, Vector3.zero, Quaternion.identity, null, pooling);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Destroy(GameObject go)//Resource안에 Object를 제거하는 함수
     {
-        
+        if (GameManager.Pool.IsContain(go))//poolContains안에 있는 경우
+            GameManager.Pool.Release(go);//Release를 통해 지워준다.
+        else
+            GameObject.Destroy(go);//위 조건이 아닐경우 제거해버린다.
+    }
+
+    public void Destroy(GameObject go, float delay)//약간의 시간을 주고 제거하는 함수
+    {                                               
+        if (GameManager.Pool.IsContain(go))
+            StartCoroutine(DelayReleaseRoutine(go, delay));//위와 다르게 코루틴을 통하여 딜레이르 주어 제거
+        else
+            GameObject.Destroy(go, delay);
+    }
+
+    IEnumerator DelayReleaseRoutine(GameObject go, float delay)//코루틴을 사용해서 딜레이 후 제거
+    {
+        yield return new WaitForSeconds(delay);//delay만큼 기다린다.
+        GameManager.Pool.Release(go);//그후 제거한다.
+    }
+
+    public void Destroy(Component component, float delay = 0f)//기본적으로 딜레이를 0으로 주고 제거하는 함수를 미리 제작해준다.
+    {
+        Component.Destroy(component, delay);
     }
 }
